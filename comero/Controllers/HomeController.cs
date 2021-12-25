b1bae32bc90a44432fec7ce1e56610b9aafcebd9
mahyar <mohammadmahdisherafat79@gmail.com>
2021-12-26 00:16:01 +0330

    end of part 34 & 33

--- comero/Controllers/HomeController.cs
+++ comero/Controllers/HomeController.cs
@@ -3,29 +3,60 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using comero.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using comero.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace comero.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private ComeroContext _context;
+        public HomeController(ILogger<HomeController> logger, ComeroContext context)
         {
+            _context = context;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var products = _context.Products.ToList();
+            return View(products);
         }
         [Route("ContactUs")]
         public IActionResult ContactUs()
         {
             return View();
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var product = _context.Products
+                .Include(p => p.Item)
+                .SingleOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            var category = _context.Products
+                .Where(p => p.Id == product.Id)
+                .SelectMany(c => c.CategoryToProducts)
+                .Select(ca => ca.Category).ToList();
+            var productData = new DetailsViewModel()
+            {
+                Product = product,
+                Categories = category
+            };
+            return View(productData);
+        }
+
+        public IActionResult AddToCart(int id)
+        {
+            return null;
         }
         public IActionResult Privacy()
         {
